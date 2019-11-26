7737dd95eee5853033b8ef3f7421ec1057bf0f6c
Serjao <Serjao@Notebook_Serjao>
2019-11-26 14:44:47 -0200

    Primeiro Commit. Criadas as classes da model com devidos atributos!

---
+++ src/classes/Disciplina.java
@@ -0,0 +1,16 @@
+package classes;
+
+
+public class Disciplina {
+    
+//  AREA PARA DECLARAÇAO DOS ATRIBUTOS
+    
+    private String nome;
+    private int cargaHoraria;
+    private String tipo;
+    private int freqMinima;
+    private Curso curso;
+    private Frequencia frequencia;
+    private Nota nota;
+    
+}
